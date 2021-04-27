05c42964d9d59d145b8b9fab0f98fa6bcc132991
Pablo Carvelli Vargas <pablo@inf-pacava.fjeclot.net>
2021-04-27 23:11:48 +0200

    M06UF4_Practica_01

--- src/pack01/Parking.java
+++ src/pack01/Parking.java
@@ -3,6 +3,20 @@ package pack01;
 import java.util.*;
 
 public class Parking {
+	
 	ArrayList<Plaza> p;
+	
+	public Parking(int motos, int coches) {
+		
+		for(int i = 0; i < motos; i++) {
+			p.add(new Plaza("Moto"));
+		}
+		
+		for(int i = 0; i < coches; i++) {
+			p.add(new Plaza("COche"));
+		}
+		
+	}
+	
 
 }
